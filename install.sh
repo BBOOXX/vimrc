01e2532633b20c18221e748b10e002cfbb5f96fa
盒子 <i@box.gd>
2017-08-04 18:59:17 +0800

    强迫症犯了..排个序

--- install.sh
+++ install.sh
@@ -11,30 +11,30 @@ wget -O ~/.vim/colors/monokai.vim https://raw.githubusercontent.com/BBOOXX/vim-m
 echo "下载插件"
 wget -O ~/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 cd ~/.vim/bundle
-git clone git://github.com/vim-airline/vim-airline
-git clone git://github.com/vim-airline/vim-airline-themes
-git clone git://github.com/kien/ctrlp.vim
-git clone git://github.com/scrooloose/nerdtree
-git clone git://github.com/davidhalter/jedi-vim
-git clone git://github.com/ervandew/supertab
-git clone git://github.com/mattn/emmet-vim
-git clone git://github.com/kien/rainbow_parentheses.vim
 git clone git://github.com/Raimondi/delimitMate
+git clone git://github.com/SirVer/ultisnips
+git clone git://github.com/davidhalter/jedi-vim
+git clone git://github.com/dhruvasagar/vim-table-mode
+git clone git://github.com/easymotion/vim-easymotion
+git clone git://github.com/ervandew/supertab
+git clone git://github.com/fisadev/vim-isort
+git clone git://github.com/honza/vim-snippets
+git clone git://github.com/iamcco/markdown-preview.vim
+git clone git://github.com/junegunn/vim-easy-align
+git clone git://github.com/kana/vim-textobj-indent
+git clone git://github.com/kana/vim-textobj-user
+git clone git://github.com/kien/ctrlp.vim
+git clone git://github.com/kien/rainbow_parentheses.vim
+git clone git://github.com/kshenoy/vim-signature
+git clone git://github.com/majutsushi/tagbar
+git clone git://github.com/mattn/emmet-vim
 git clone git://github.com/scrooloose/nerdcommenter
+git clone git://github.com/scrooloose/nerdtree
 git clone git://github.com/scrooloose/syntastic
 git clone git://github.com/tell-k/vim-autopep8
-git clone git://github.com/SirVer/ultisnips
-git clone git://github.com/honza/vim-snippets
-git clone git://github.com/majutsushi/tagbar
-git clone git://github.com/iamcco/markdown-preview.vim
-git clone git://github.com/easymotion/vim-easymotion
+git clone git://github.com/vim-airline/vim-airline
+git clone git://github.com/vim-airline/vim-airline-themes
 git clone git://github.com/vim-scripts/DrawIt
-git clone git://github.com/kshenoy/vim-signature
-git clone git://github.com/dhruvasagar/vim-table-mode
-git clone git://github.com/junegunn/vim-easy-align
-git clone git://github.com/fisadev/vim-isort
-git clone git://github.com/kana/vim-textobj-user
-git clone git://github.com/kana/vim-textobj-indent
 cd ~
 echo "通过pip安装依赖"
 sudo pip install jedi autopep8 isort
