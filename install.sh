64d34ba60d5bc2fbbaa9a09ebf24d75b08bc35d4
盒子 <i@box.gd>
2022-05-02 18:49:27 +0800

    增加 termux环境检查

--- install.sh
+++ install.sh
@@ -3,12 +3,18 @@ cd $(dirname $0)
 export BASE=$(pwd)
 export VIMRC_INIT=true
 
+case "$PREFIX" in
+    *com.termux*) termux=true ;;
+    *) termux=false ;;
+esac
+
 case "$OSTYPE" in
-    darwin*)  brew install vim wget cmake;;
-    linux*)   sudo apt install -y vim-nox wget jq cmake build-essential python3-dev libjansson-dev automake pkg-config
-              git clone https://github.com/universal-ctags/ctags.git --depth=1
-              cd ctags && ./autogen.sh && ./configure && make && sudo make install && cd ..
-        ;;
+    darwin*)  brew install vim wget cmake ;;
+    linux*)   if [ "$termux" != true ]; then
+                sudo apt install -y git vim-nox wget jq cmake build-essential python3-dev libjansson-dev automake pkg-config
+                git clone https://github.com/universal-ctags/ctags.git --depth=1
+                cd ctags && ./autogen.sh && ./configure && make && sudo make install && cd -
+              fi ;;
     *)        echo "unknown: OS: $OSTYPE, U should install dependences by yourself" ;;
 esac
 
